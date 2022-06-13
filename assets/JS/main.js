f182c38d1d16be3c0a4e2db16663758db98b780e
AlexHK7 <alexandre.maire13@outlook.fr>
2022-06-13 10:14:48 +0200

    LightMode

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,1 +1,13 @@
-console.log("helloworld");
+let isLight = false
+
+function Light() {
+if (isLight == true) {
+    document.querySelector("link").href = "assets/css/style_light.css"
+}else{
+    document.querySelector("link").href = "assets/css/style.css"
+
+}
+isLight = !isLight
+}
+
+
